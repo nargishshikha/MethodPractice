bf79b19aab0a147fd6a16ef1a3ee63f71eba7758
nargishshikha <mst.nargish@gmail.com>
2018-02-03 09:55:01 -0800

    add method exercise-1

--- src/exercise/Methods.java
+++ src/exercise/Methods.java
@@ -3,9 +3,24 @@ package exercise;
 public class Methods {
 	
 	public static void main(String[] args) {
-		
+
 		System.out.println("Hello Gidhub");
-		
+		System.out.println("Que-1: " + add(10, 30));
+	}
+
+	/**Que-1: Write a method that take two number input and return the sum.
+	 * 
+	 * parameter: int numOne, int numTwo
+	 * returnType: int
+	 * servingBucket: variable
+	 */
+
+	public static int add(int numOne, int numTwo) {
+		int result = 0;
+
+		result = numOne + numTwo;
+
+		return result;
 	}
 
 }
